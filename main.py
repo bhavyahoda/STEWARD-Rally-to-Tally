3da83a999498b7cf5f389535978b966bca545ea2
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2021-09-16 00:49:44 +0530

    Adds circle detection from system video capture
    - Adds venv and DS store to gitignore

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import cv2
+import numpy as np
 
 system_video_capture = cv2.VideoCapture(0)
 
 while True:
-	ret, frame = system_video_capture.read()
-	cv2.imshow("Frame", frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    ret, frame = system_video_capture.read()
+    gray = cv2.medianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 5)
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 system_video_capture.release()
 cv2.destroyAllWindows()
