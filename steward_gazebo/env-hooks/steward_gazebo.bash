7fc58d414fef094f279843ceb15bfd7bb22f12aa
Jasmeet Singh <jasmeet0915@gmail.com>
2021-09-24 00:47:03 +0530

    Adds battery plugin to gazebo_plugin_path

--- steward_gazebo/env-hooks/steward_gazebo.bash
+++ steward_gazebo/env-hooks/steward_gazebo.bash
@@ -2,3 +2,4 @@
 
 source /usr/share/gazebo/setup.bash
 export GAZEBO_MODEL_PATH="${CATKIN_ENV_HOOK_WORKSPACE}/../src/steward/steward_gazebo/models/:${GAZEBO_MODEL_PATH}"
+export GAZEBO_PLUGIN_PATH="${CATKIN_ENV_HOOK_WORKSPACE}/../src/brass_gazebo_battery/build/devel/lib/:${GAZEBO_MODEL_PATH}"
