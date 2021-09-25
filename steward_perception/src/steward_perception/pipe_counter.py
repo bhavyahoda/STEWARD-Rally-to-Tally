126f995b731819d9fb9c8eb7fcc4064859a93d67
Jasmeet Singh <jasmeet0915@gmail.com>
2021-09-26 04:12:11 +0530

    Adds stack stats publisher to pipe_counter node

--- steward_perception/src/steward_perception/pipe_counter.py
+++ steward_perception/src/steward_perception/pipe_counter.py
@@ -1,16 +1,18 @@
 #! /home/singh/.virtualenvs/cv/bin/python
 
+from inspect import stack
 import rospy
 import cv2
-import dlib
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from rospy.exceptions import ROSException
+from rospy.timer import sleep
 from sensor_msgs.msg import Image
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, Int32
 from Utils.circle_detection import get_centers
 from Utils.centroid_tracker import CentroidTracker
 from Utils.trackable_object import TrackableObject
+from steward_perception.msg import StackStats
 
 
 class PipeCounter:
@@ -20,6 +22,7 @@ class PipeCounter:
         # maybe wont need this if we use contour detection directly to detect stack
         # else this used to start counting by ar_tag/qr_code detection script 
         self.start_counting_sub = rospy.Subscriber("/pipe_counter/start", Empty, self.start_counting_cb)
+        self.stop_counting_pub = rospy.Subscriber("/pipe_counter/stop", Empty, self.stop_counting_cb)
         rospy.loginfo("pipe_counter: subscriber created") 
         
         self.img_bridge = CvBridge()
@@ -30,7 +33,29 @@ class PipeCounter:
         self.trackers = []
         self.tracked_objects = {}
 
+        self.stacks = {
+            'A': {
+                'pipe_od': '48inch',
+                'pipe_manu_type': 'seamless',
+                'pipe_material': 'steel'
+            },
+            'B': {
+                'pipe_od': '24inch',
+                'pipe_manu_type': 'seamed',
+                'pipe_material': 'steel'
+            },
+            'C': {
+                'pipe_od': '36inch',
+                'pipe_manu_type': 'seamed',
+                'pipe_material': 'steel'
+            }
+        }
+
+        self.stack_counted = 0
+        self.mission = '26 September 2021'
+
         self.image_pub = rospy.Publisher("/pipe_counter/processed_image", Image, queue_size=10)
+        self.stack_stats_pub = rospy.Publisher("/pipe_counter/stack_stats", StackStats, queue_size=10)
 
     def image_cb(self, image):
         try:
@@ -40,7 +65,6 @@ class PipeCounter:
 
         frame_rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
         rects = []
-        #cv_image = cv_image[:, cv_image.shape[1]//2-10:]
         
         if self.total_frames % self.skip_frames == 0:
             self.trackers = []
@@ -54,23 +78,6 @@ class PipeCounter:
                     endX = x + r
                     endY = y + r
                     rects.append((startX, startY, endX, endY))
-
-                    #tracker = dlib.correlation_tracker()
-                    #rect = dlib.rectangle(startX, startY, endX, endY)
-                    #tracker.start_track(frame_rgb, rect)
-#
-                    #self.trackers.append(tracker)
-        #else:
-        #    for tracker in self.trackers:
-        #        tracker.update(frame_rgb)
-        #        pos = tracker.get_position()
-#
-        #        startX = int(pos.left())
-        #        startY = int(pos.top())
-        #        endX = int(pos.right())
-        #        endY = int(pos.bottom())
-#
-        #        rects.append((startX, startY, endX, endY))
 
         cv2.line(cv_image, 
                 (cv_image.shape[1]//2, 0), 
@@ -116,6 +123,23 @@ class PipeCounter:
         self.count = 0
 
         self.image_sub = rospy.Subscriber("/image", Image, self.image_cb)
+
+    def stop_counting_cb(self, msg):
+        rospy.loginfo("pipe_counter: STOPPED COUNTING STACK")
+
+        stack_stats_msg = StackStats()
+        stack_stats_msg.id = self.stacks[self.stack_counted]['id']
+        stack_stats_msg.count = self.count
+        stack_stats_msg.mission = self.mission
+        stack_stats_msg.pipe_manu_type = self.stacks[self.stack_counted]['pipe_manu_type']
+        stack_stats_msg.pipe_material = self.stacks[self.stack_counted]['pipe_material']
+        stack_stats_msg.pipe_od = self.stacks[self.stack_counted]['pipe_od']
+
+        self.stack_stats_pub.publish(stack_stats_msg)
+
+        rospy.loginfo("pipe_counter: Counted Stack id: {}".format(self.stacks[self.stack_counted]['id']))
+
+        self.stack_counted += 1
 
 
 if __name__ == "__main__":
